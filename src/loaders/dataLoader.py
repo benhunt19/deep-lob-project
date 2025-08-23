c30ee5359c9f513e3a27fc640e8185497a9def25
Ben Hunt <ben.hunt19@hotmail.com>
2025-08-24 00:07:11 +0100

    adding new updates to metrics

--- src/loaders/dataLoader.py
+++ src/loaders/dataLoader.py
@@ -299,7 +299,7 @@ class CustomDataLoader:
         normalised = np.clip(normalised, -clip_thresh, clip_thresh)
         
         # Return as a column vector
-        print(f"Labels Greater than zero: {np.sum(normalised > 0)}, less than zero: {np.sum(normalised < 0)}")
+        print(f"Labels Greater than zero: {np.sum(normalised > 0)}, less than zero: {np.sum(normalised < 0)}, zero: {np.sum(normalised == 0)}")
         return normalised.reshape(-1, 1)
     
     def splitDataTrainTest(self):
